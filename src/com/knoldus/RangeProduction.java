7b349113c4ecbdf387a20bc071afc65b3950c191
Kuldeep <76957624+Kuldeep-knoldus@users.noreply.github.com>
2021-03-08 20:11:15 +0530

    Update RangeProduction.java

--- src/com/knoldus/RangeProduction.java
+++ src/com/knoldus/RangeProduction.java
@@ -6,16 +6,19 @@ import java.util.stream.LongStream;
 
 public class RangeProduction {
     public static void main(String[] args) {
-    LongBinaryOperator operator = (x, y) -> LongStream.rangeClosed(x, y)
-            .reduce((num1, num2) -> num1 * num2)
-            .orElse(1l);
-
-
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Insert Range: ");
-        long input1=scanner.nextLong();
-        long input2=scanner.nextLong();
-        Long result = operator.applyAsLong(input1, input2);
-        System.out.println(result);
-}
+        long input1 = scanner.nextLong();
+        long input2 = scanner.nextLong();
+
+        System.out.println(getRangeProduction(input1, input2));
+    }
+
+    public static Long getRangeProduction(long i1, long i2) {
+        LongBinaryOperator operator = (x, y) -> LongStream.rangeClosed(x, y)
+                .reduce((num1, num2) -> num1 * num2)
+                .orElse(1l);
+        Long result = operator.applyAsLong(i1, i2);
+        return result;
+    }
 }
